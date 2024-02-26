3a6a6cea94abeeea88c286d9b5cb06cf4f32dd45
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2024-02-26 01:35:52 -0300

    components e styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CardHorizontalComponent } from './components/card-horizontal/card-horizontal.component';
+import { CardVerticalComponent } from './components/card-vertical/card-vertical.component';
+import { CardInfoComponent } from './components/card-info/card-info.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    BannerComponent,
+    CardHorizontalComponent,
+    CardVerticalComponent,
+    CardInfoComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-vertical/card-vertical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-vertical',
+  templateUrl: './card-vertical.component.html',
+  styleUrls: ['./card-vertical.component.css']
+})
+export class CardVerticalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
